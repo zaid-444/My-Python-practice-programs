6e83c364805fdd02c971b2769844bfc8abe93944
Zaid <zaid0999@gmail.com>
2026-02-18 14:58:33 +0530

    Packages

---
+++ Module's/Packag's/Pack2/icicidemo.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append("D:\Python NareshIT(2024) KV Rao sir\Problems\Module's\Packag's")
+from Pack1.icici import bname,addr,simpleint
+
+print("Banck Name: {}".format(bname))
+print("Banck Addr: {}".format(addr))
+simpleint()
