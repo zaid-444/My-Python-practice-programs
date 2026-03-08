6942d0a12079882f48e935d2392079009019d712
Zaid <zaid0999@gmail.com>
2026-03-08 17:37:18 +0530

    ,

--- DataBase's/OracleDB/OrTableCreateEx1.py
+++ DataBase's/OracleDB/OrTableCreateEx1.py
@@ -5,7 +5,7 @@ try:
     con = orc.connect("system/tiger@localhost/orcl")
     cur = con.cursor()
 
-    cq = "create table employee(eno number(2) primary key, name varchar(10) not null, marks number(5,2) not null)"
+    cq = "create table employee(eno number(2) primary key, name varchar(10) not null, sal number(5,2) not null)"
 
     cur.execute(cq)
 
